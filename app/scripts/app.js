c950a548c6ce28f268e9374e93255540e8bafcf2
Ivan L. Gatnau <ilgatnau@gmail.com>
2014-11-30 12:36:49 +0100

    simpleWeather jquery plugin added

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version',
+  'oauth'
+]);
+
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+
+  $routeProvider.when('/access_token=:accessToken', {
+      template: '',
+      controller: function ($location, AccessToken) {
+        var hash = $location.path().substr(1);
+        AccessToken.setTokenFromString(hash);
+        $location.path('/');
+        $location.replace();
+      }
+    })
+}]);
+
