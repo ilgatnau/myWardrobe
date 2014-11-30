a3997ec9c4c63bc8aed488f2705be45040d27276
Ivan L. Gatnau <ilgatnau@gmail.com>
2014-11-30 11:53:59 +0100

    Some changes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,46 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+  'myApp.version',
+  'oauth'
+]);
+
+
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+
+  $routeProvider.when('/access_token=:accessToken', {
+      template: '',
+      controller: function ($location, AccessToken) {
+        var hash = $location.path().substr(1);
+        AccessToken.setTokenFromString(hash);
+        $location.path('/');
+        $location.replace();
+      }
+    })
 }]);
+
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'San Juan Despi',
+    woeid: '',
+    unit: 'c',
+    units.speed: 'kph',
+    success: function(weather) {
+      var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+
