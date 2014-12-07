f2364995a13a3a44583dad47152cb38d5f3bd649
Ivan L. Gatnau <ilgatnau@gmail.com>
2014-12-07 15:04:12 +0100

    Instagram token expired workflow

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,9 +23,10 @@ app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 }]);
 
-app.controller('appCtrl', function($scope, $http, $rootScope) {
-  $scope.loggedin = false;
-  $scope.user = {
+app.controller('appCtrl', function($scope, $http, $rootScope, $sessionStorage) {
+
+  $rootScope.loggedin = false;
+  $rootScope.user = {
     id : "",
     username : "",
     token: ""
@@ -36,9 +37,9 @@ app.controller('appCtrl', function($scope, $http, $rootScope) {
     console.log('Authorized third party app with token', token.access_token);
 
     // Set global token value;
-    $rootScope.user_token = token.access_token;
+    $rootScope.user.token = token.access_token;
 
-    var url = "https://api.instagram.com/v1/users/self?access_token=" + token.access_token + "&callback=JSON_CALLBACK";
+    var url = "https://api.instagram.com/v1/users/self?access_token=" + $rootScope.user.token + "&callback=JSON_CALLBACK";
     
     //Access-Control-Allow-Origin error, required jsonp requests
 
@@ -53,27 +54,32 @@ app.controller('appCtrl', function($scope, $http, $rootScope) {
         .success(function(returnJsonp){
             console.log(returnJsonp);
 
-      $scope.user.id = returnJsonp.data.id;
-      $scope.user.username = returnJsonp.data.username;
-      $scope.user.token = token.access_token;
+      $rootScope.user.id = returnJsonp.data.id;
+      $rootScope.user.username = returnJsonp.data.username;
+      $rootScope.user.token = token.access_token;
 
-      $rootScope.user_id = returnJsonp.data.id;
-      console.log($rootScope.user_id);
-      console.log($rootScope.user_token);
+      console.log($rootScope.user.id);
+      console.log($rootScope.user.token);
     });
 
   });
 
   $scope.$on('oauth:logout', function(event) {
     console.log('The user has signed out');
+
+    $rootScope.user.token = null;
   });
 
   $scope.$on('oauth:denied', function(event) {
     console.log('The user did not authorize the third party app');
+    $rootScope.user.token = null;
   });
 
   $scope.$on('oauth:expired', function(event) {
-    console.log('The access token is expired. Please refresh.');
+    console.log(event.targetScope.user.token)
+    var url = "https://api.instagram.com/v1/users/self?access_token=" + event.targetScope.user.token + "&callback=JSON_CALLBACK";
+    console.log(url);
+    //window.location.replace(url);
   });
 
   $scope.$on('oauth:profile', function(profile) {
